f14e16fa9c234dc6b92d70a5487543101d9292f4
Jose Diaz <20163861@itla.edu.do>
2025-05-16 11:53:01 -0400

    Subiendo el inicio del programa del SGB

---
+++ SGB/AplicacionDeDatos.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace SGB
+{
+    internal class BibliotecaBDContext : DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var config = new ConfigurationBuilder()
+                 .SetBasePath(Directory.GetCurrentDirectory())
+                 .AddJsonFile("appsettings.json")
+                 .Build();
+
+            var connectionString = config.GetConnectionString("DefaultConnection");
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
---
+++ SGB/Form1.cs
@@ -0,0 +1,29 @@
+namespace SGB
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeComponent();
+            if (!ProbarConexion())
+            {
+                MessageBox.Show("Error al conectar con la base de datos. La aplicación se cerrará.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+               
+            }
+
+        }
+        private bool ProbarConexion()
+        {
+            try
+            {
+                var db = new BibliotecaBDContext();
+                return db.Database.CanConnect();
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
